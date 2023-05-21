713b9de370a1c6c057f077c0da644137f2bed26a
Bruno Kraychete da Costa <1355373+brunokc@users.noreply.github.com>
2023-05-20 17:20:42 -0700

    Add script for local build

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME=pve-kernel-build
+
+echo Preparing container...
+docker build \
+    --build-arg DEBIAN_RELEASE=${DEBIAN_RELEASE} \
+    --build-arg REPO_URL=${REPO_URL} \
+    --build-arg REPO_BRANCH=${REPO_BRANCH} \
+    -t ${IMAGE_NAME} .
+
+echo Building PVE kernel...
+docker run -v $(pwd)/output:/output ${IMAGE_NAME} scripts/build-kernel.sh
