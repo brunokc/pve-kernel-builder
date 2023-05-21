74685310e587aee2af586a45c383a197a2890a9b
Bruno Kraychete da Costa <1355373+brunokc@users.noreply.github.com>
2023-05-21 01:06:57 -0700

    Suppress debugging messages

--- scripts/extract_proxmox_branch.sh
+++ scripts/extract_proxmox_branch.sh
@@ -8,10 +8,10 @@ echoerr() {
 
 re='config/([^/]+)/version'
 for commit in `yq '.commits[] as $c | $c.id' -`; do
-    echoerr "Commit: ${commit}"
+    #echoerr "Commit: ${commit}"
     affected_files=$(git log --format= --name-status -n 1 $commit | cut -f 2)
     for file in $affected_files; do
-        echoerr "analyzing $file"
+        #echoerr "analyzing $file"
         if [[ $file =~ $re ]]; then
             echo "${BASH_REMATCH[1]}"
         fi
