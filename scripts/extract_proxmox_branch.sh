1df326790d4c7d13b773036790c6658c14bbcc97
Bruno Kraychete da Costa <1355373+brunokc@users.noreply.github.com>
2023-05-20 23:59:58 -0700

    Extract proxmox branch from affected version file

---
+++ scripts/extract_proxmox_branch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echoerr() {
+    printf "%s\n" "$*" >&2
+}
+
+re='config/([^/]+)/version'
+for commit in `yq '.commits[] as $c | $c.id' -`; do
+    echoerr "Commit: $commit"
+    affected_files=$(git log --format= --name-status -n 1 $c | cut -f 2)
+    for file in $affected_files; do
+        echoerr "analyzing $file"
+        if [[ $file =~ $re ]]; then
+            echo "branch=${BASH_REMATCH[1]}"
+        fi
+    done
+done
