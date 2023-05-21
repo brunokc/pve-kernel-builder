cba1f29e6a96e838b8e7b77c79b805904845782d
Bruno Kraychete da Costa <1355373+brunokc@users.noreply.github.com>
2023-05-21 00:40:06 -0700

    Don't print out variable assignment
    Do the assignment on the workflow itself.

--- scripts/extract_proxmox_branch.sh
+++ scripts/extract_proxmox_branch.sh
@@ -6,12 +6,12 @@ echoerr() {
 
 re='config/([^/]+)/version'
 for commit in `yq '.commits[] as $c | $c.id' -`; do
-    echoerr "Commit: $commit"
+    echoerr "Commit: ${commit}"
     affected_files=$(git log --format= --name-status -n 1 $c | cut -f 2)
     for file in $affected_files; do
         echoerr "analyzing $file"
         if [[ $file =~ $re ]]; then
-            echo "branch=${BASH_REMATCH[1]}"
+            echo "${BASH_REMATCH[1]}"
         fi
     done
 done
