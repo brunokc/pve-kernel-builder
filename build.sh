a7b86d44cbf8e896e6a597a0a29c55692ec29a2b
Bruno Kraychete da Costa <1355373+brunokc@users.noreply.github.com>
2023-05-23 23:02:47 +0000

    Accept build information as arguments

--- build.sh
+++ build.sh
@@ -1,5 +1,40 @@
 #!/usr/bin/env bash
 
+while getopts ':d:u:b:' OPTION; do
+    case "$OPTION" in
+        d)
+            DEBIAN_RELEASE=$OPTARG
+            ;;
+        u)
+            REPO_URL=$OPTARG
+            ;;
+        b)
+            REPO_BRANCH=$OPTARG
+            ;;
+        ?|h|help)
+            echo "Usage: $(basename $0) [-d <DEBIAN_RELEASE>] [-u <REPO_URL>] -b <REPO_BRANCH>"
+            echo
+            echo "Ex: $(basename $0) -d bullseye -u git://git.proxmox.com/git/pve-kernel.git -b master"
+            exit 1
+            ;;
+    esac
+done
+
+DEBIAN_RELEASE=${DEBIAN_RELEASE:-bullseye}
+REPO_URL=${REPO_URL:-git://git.proxmox.com/git/pve-kernel.git}
+
+if [ -z "$REPO_BRANCH" ]; then
+    echo "Branch name required. Please specify the branch to build with the -b option."
+    echo "Type \"$(basename $0) -h\" for usage."
+    exit 1
+fi
+
+
+echo Using:
+echo DEBIAN_RELEASE=$DEBIAN_RELEASE
+echo REPO_URL=$REPO_URL
+echo REPO_BRANCH=$REPO_BRANCH
+
 IMAGE_NAME=pve-kernel-build
 
 echo Preparing container...
