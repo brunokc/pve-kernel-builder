59b4916b03e162eb0757fb17ac6f02d72e9eb85a
Bruno Kraychete da Costa <1355373+brunokc@users.noreply.github.com>
2023-05-23 07:27:49 -0700

    Move output to the builder account's home

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 echo -e "Cores available: $(nproc)"
 cd /build/pve-kernel
 
@@ -7,12 +9,12 @@ echo Building kernel...
 make
 
 echo Exporting artifacts
-mkdir -p /output/artifacts
-cp *.deb /output/artifacts
+mkdir -p output/artifacts
+cp *.deb output/artifacts
 
 for d in build pve-kernel-*; do
     if [[ -d $d ]]; then
         echo "found abi files in $d"
-        cp $d/abi* /output
+        cp $d/abi* output
     fi
 done
